bc000c7b4ccb2141dd0b55cf2d2abd39922201ce
Andrés Ruda <carloarm@gmail.com>
2025-02-13 18:33:13 -0300

    + Go7

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,6 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'stultifera';
+  anio: number = new Date().getFullYear();
+  author: string = '<b>@corpus</b><br>' + this.anio;
 }
