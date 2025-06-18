0565a48b42ee2d73e92f4ba467aece4e101986b3
Andrés Ruda <carloarm@gmail.com>
2025-06-18 18:41:31 -0400

    + Add plank

---
+++ src/app/plank/plank.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-plank',
+  imports: [],
+  templateUrl: './plank.component.html',
+  styleUrl: './plank.component.css'
+})
+export class PlankComponent {
+
+}
